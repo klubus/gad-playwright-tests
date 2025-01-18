f9fe1f08e11f7bf74e1129ab324bec0319f77e56
Krystian Kluba <klubakrystian@gmail.com>
2025-01-18 11:07:41 +0100

    chore: eslint with playwright

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,17 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import eslintPluginPlaywright from 'eslint-plugin-playwright';
-
-/** @type {import('eslint').Linter.Config[]} */
 export default [
   { ignores: ['package-lock.json', 'playwright-report/**', 'test-results/**'] },
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { languageOptions: { globals: globals.browser } },
+  { files: ['**/*.ts'] },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     rules: {
-      'no-console': 'warn',
+      'no-console': 'error',
     },
   },
   {
@@ -19,6 +19,18 @@ export default [
       '@typescript-eslint/explicit-function-return-type': 'error',
     },
   },
-  ...tseslint.configs.recommended,
-  eslintPluginPlaywright,
+  eslintPluginPlaywright.configs['flat/recommended'],
+  {
+    rules: {
+      'playwright/no-nested-step': 'off',
+    },
+    settings: {
+      playwright: {
+        globalAliases: {
+          test: ['setup'],
+        },
+      },
+    },
+  },
+  eslintPluginPrettierRecommended,
 ];
