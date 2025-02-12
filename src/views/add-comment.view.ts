3fa0e88390baedb688c055b15c43a243225b7f41
Krystian Kluba <klubakrystian@gmail.com>
2025-02-12 19:38:25 +0100

    wip: comment lifecycle, edit comment

--- src/views/add-comment.view.ts
+++ src/views/add-comment.view.ts
@@ -1,3 +1,4 @@
+import { AddCommentModel } from '../models/comment.model';
 import { Locator, Page } from '@playwright/test';
 
 export class AddCommentView {
@@ -11,5 +12,10 @@ export class AddCommentView {
     });
     this.bodyInput = this.page.locator('#body');
     this.saveButton = this.page.getByRole('button', { name: 'Save' });
+  }
+
+  async createComment(commentData: AddCommentModel): Promise<void> {
+    this.bodyInput.fill(commentData.body);
+    this.saveButton.click();
   }
 }
