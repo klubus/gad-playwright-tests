f5661bf06fd743fa44673f39a67308b34f82cb10
Krystian Kluba <klubakrystian@gmail.com>
2025-02-25 18:47:08 +0100

    wip: session storage handling in projects

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,13 @@
 import { BASE_URL } from './src/env.config';
 import { defineConfig, devices } from '@playwright/test';
+import * as path from 'path';
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
+
+export const STORAGE_STATE = path.join(__dirname, 'tmp/session.json');
+
 export default defineConfig({
   testDir: './tests',
   globalSetup: 'src/global-setup.ts',
@@ -25,6 +29,7 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
+      grepInvert: /@logged/,
     },
     {
       name: 'setup',
@@ -34,6 +39,9 @@ export default defineConfig({
       name: 'logged',
       grep: /@logged/,
       dependencies: ['setup'],
+      use: {
+        storageState: STORAGE_STATE,
+      },
     },
   ],
 });
--- tests/articles.spec.ts
+++ tests/articles.spec.ts
@@ -1,30 +1,24 @@
 import { prepareRandomArticle } from '../src/factories/article.factory';
 import { ArticlePage } from '../src/pages/article.page';
 import { ArticlesPage } from '../src/pages/articles.page';
-import { LoginPage } from '../src/pages/login.page';
-import { testUser1 } from '../src/test-data/user.data';
 import { AddArticleView } from '../src/views/add-article.view';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify articles', () => {
-  let loginPage: LoginPage;
   let articlesPage: ArticlesPage;
   let addArticleView: AddArticleView;
 
   test.beforeEach(async ({ page }) => {
-    loginPage = new LoginPage(page);
     articlesPage = new ArticlesPage(page);
     addArticleView = new AddArticleView(page);
 
-    await loginPage.goto();
-    await loginPage.login(testUser1);
     await articlesPage.goto();
 
     await articlesPage.addArticleButtonLogged.click();
     await expect.soft(addArticleView.addNewHeader).toBeVisible();
   });
 
-  test('reject creating article without title @GAD_R04_01', async ({}) => {
+  test('reject creating article without title @GAD_R04_01 @logged', async ({}) => {
     // Arrange
     const expectedErrorMessage = 'Article was not created';
     const articleData = prepareRandomArticle();
@@ -37,7 +31,7 @@ test.describe('Verify articles', () => {
     await expect(addArticleView.alertPopUp).toHaveText(expectedErrorMessage);
   });
 
-  test('reject creating article without body @GAD_R04_01', async ({}) => {
+  test('reject creating article without body @GAD_R04_01 @logged', async ({}) => {
     // Arrange
     const expectedErrorMessage = 'Article was not created';
     const articleData = prepareRandomArticle();
@@ -51,7 +45,7 @@ test.describe('Verify articles', () => {
   });
 
   test.describe('title length', () => {
-    test('reject creating article with title exceeding 128 signs @GAD_R04_02', async ({}) => {
+    test('reject creating article with title exceeding 128 signs @GAD_R04_02 @logged', async ({}) => {
       // Arrange
       const expectedErrorMessage = 'Article was not created';
       const articleData = prepareRandomArticle(129);
@@ -63,7 +57,7 @@ test.describe('Verify articles', () => {
       await expect(addArticleView.alertPopUp).toHaveText(expectedErrorMessage);
     });
 
-    test('create article with title with 128 signs @GAD_R04_02', async ({
+    test('create article with title with 128 signs @GAD_R04_02 @logged', async ({
       page,
     }) => {
       // Arrange
